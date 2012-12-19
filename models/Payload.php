c9d1145d75716fb63da9e7aa323265f77b64bdab
Gary Rafferty <gary.rafferty@gmail.com>
2012-12-19 12:16:05 +0000

    Model for the XML payload

---
+++ models/Payload.php
@@ -0,0 +1,13 @@
+<?php
+  namespace li3_airbrake\models;
+
+  class Payload {
+
+    private $config;
+    private $exception;
+
+    public function __construct($exception,$config) {
+      //build the xml payload
+    }
+  }
+?>
