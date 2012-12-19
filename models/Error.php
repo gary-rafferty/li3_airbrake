1edfab198e866ad6743dc4d81dd0ffce2d45f0da
Gary Rafferty <gary.rafferty@gmail.com>
2012-12-19 16:57:44 +0000

    Flesh out Error class to construct a basic error

--- models/Error.php
+++ models/Error.php
@@ -3,8 +3,32 @@
 
   class Error {
 
-    public function __construct($exception) {
+    public $exception;
+    public $message;
+    public $request;
+    public $url;
+    public $controller;
+    public $action;
+    public $trace = array();
+    public $cgi = array();
 
+    public function __construct($exception) {
+      $this->exception = get_class($exception->exception);
+      $this->message = $exception->message;
+      $this->request = $exception->trace[0]['args'][0]->request;
+      $this->url = $exception->trace[0]['args'][0]->request->url;
+      $this->controller = $exception->trace[0]['args'][0]->request->controller;
+      $this->action = $exception->trace[0]['args'][0]->request->action;
+      foreach($exception->trace as $trace) {
+        $this->trace[] = array(
+          $trace['file'],
+          $trace['line'],
+          $trace['function']
+        );
+      }
+      foreach($_SERVER as $key => $val) {
+        $this->cgi[$key] = $val;
+      }
     }
   }
 ?>
