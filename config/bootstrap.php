b5258466ae14014f3a9c09705fcc6dc75cdd108c
Gary Rafferty <gary.rafferty@gmail.com>
2012-12-19 10:10:36 +0000

    Initial commit

---
+++ config/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+use lithium\core\Libraries;
+
+if(!array_key_exists('apikey', Libraries::get('li3_airbrake'))) {
+  throw new RuntimeException('You must supply and API key');
+}
+
+$apiKey = Libraries::get('li3_airbrake','apikey');
+
+?>
