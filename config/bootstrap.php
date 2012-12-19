19720468bac6e94ee2c7df6b2d57ffdd2f556012
Gary Rafferty <gary.rafferty@gmail.com>
2012-12-19 11:39:10 +0000

    Invoke Airbrake to handle errors

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,11 +1,22 @@
 <?php
 
+require __DIR__ . '/../models/Airbrake.php';
+
 use lithium\core\Libraries;
+use lithium\core\ErrorHandler;
 
 if(!array_key_exists('apikey', Libraries::get('li3_airbrake'))) {
   throw new RuntimeException('You must supply an API key');
 }
 
 $apiKey = Libraries::get('li3_airbrake','apikey');
+
+$airbrake = new Airbrake($apiKey);
+
+ErrorHandler::apply(
+  'lithium\action\Dispatcher::run', array(), function($exception, $params) use ($airbrake) {
+    $airbrake->handle($exception,$params);
+  }
+);
 
 ?>
